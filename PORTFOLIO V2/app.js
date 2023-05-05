193da935618e0e5d21eb90338e56c96e49688842
Michael Jeffrey Papas <118707506+BuildMeAsite@users.noreply.github.com>
2023-05-05 12:10:47 +0800

    Add files via upload

---
+++ PORTFOLIO V2/app.js
@@ -0,0 +1,14 @@
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('shows')
+        } else {
+            entry.target.classList.remove('shows')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hiddensec')
+hiddenElements.forEach((el) => observer.observe(el))
